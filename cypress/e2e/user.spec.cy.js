dde256060f59956df267d5ac0bf26b7b726a28cf
Gabriel <gabrielmcarneiro17@gmail.com>
2025-08-01 11:59:34 -0300

    primeiro commit

---
+++ cypress/e2e/user.spec.cy.js
@@ -0,0 +1,47 @@
+import ListasPage from "../pages/listasPage"
+import TarefasPage from "../pages/tarefasPage"
+
+const listasPage = new ListasPage()
+const tarefasPage = new TarefasPage()
+
+describe('Testes Kukac', () => {  
+
+  it('Adicionando nova lista', () => {
+    listasPage.acessarPaginaInicial()
+    listasPage.criarNovaLista()
+    listasPage.checarListaCriada()
+  })
+
+  it('Excluindo uma lista', () => {
+    listasPage.acessarPaginaInicial()
+    listasPage.apagarLista()
+    listasPage.checarAusenciaDeLista()
+  })
+
+  it('Adicionando nova tarefa', () => {
+    listasPage.acessarPaginaInicial()
+    tarefasPage.criarNovaTarefa()
+    tarefasPage.checarTarefaCriada()
+  })
+
+  it('Editando o título de uma tarefa', () => {
+    listasPage.acessarPaginaInicial()
+    tarefasPage.clicarNaTarefa()
+    tarefasPage.editarTituloDaTarefa()
+    tarefasPage.checarTituloEditado()
+  })
+
+  it('Criando tag para uma tarefa', () => {
+    listasPage.acessarPaginaInicial()
+    tarefasPage.clicarNaTarefa()
+    tarefasPage.criarNovaTag()
+    tarefasPage.checarTagCriada()
+  })
+
+  it('Apagando uma tarefa', () => {
+    listasPage.acessarPaginaInicial()
+    tarefasPage.apagarTarefa()
+    tarefasPage.checarAusenciaDeTarefa()
+  })
+
+})
